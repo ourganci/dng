22e7f26bceaf002a16a32c2e7d676dc347dd4639
napster <oezden_u@hotmail.de>
2025-10-23 23:34:23 +0200

    Auto-generated commit

---
+++ chunk-U5T2O6VA.js
@@ -0,0 +1,1 @@
+import"./chunk-4CLCTAJ7.js";var e=[{path:"",loadComponent:()=>import("./chunk-OOG2UWUD.js").then(o=>o.ServicesOverviewComponent)},{path:":id",loadComponent:()=>import("./chunk-CSYTNPCV.js").then(o=>o.ServiceDetailComponent)}];export{e as SERVICES_ROUTES};
