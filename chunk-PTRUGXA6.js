abf80750186ddaa4ead45a7566d49ee8ea6293e9
napster <oezden_u@hotmail.de>
2025-10-26 01:04:34 +0200

    Auto-generated commit

---
+++ chunk-PTRUGXA6.js
@@ -0,0 +1,1 @@
+import"./chunk-4CLCTAJ7.js";var e=[{path:"",loadComponent:()=>import("./chunk-Y7AEJPYA.js").then(o=>o.ServicesOverviewComponent)},{path:":id",loadComponent:()=>import("./chunk-ML44BV4V.js").then(o=>o.ServiceDetailComponent)}];export{e as SERVICES_ROUTES};
