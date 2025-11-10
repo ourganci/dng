aa488e4e9f19d07a2108a91604171b26b613ad91
napster <oezden_u@hotmail.de>
2025-11-10 13:11:19 +0100

    Auto-generated commit

--- chunk-Z2ZQSXXQ.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-4CLCTAJ7.js";var t=[{path:"",loadComponent:()=>import("./chunk-54NEFC6E.js").then(o=>o.BlogComponent)},{path:":slug",loadComponent:()=>import("./chunk-KDH66GX3.js").then(o=>o.BlogDetailComponent)}];export{t as BLOG_ROUTES};
